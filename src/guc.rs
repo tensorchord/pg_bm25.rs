eba58430800ab3884a446ffa69398fc84037a667
Mingzhuo Yin <yinmingzhuo@gmail.com>
2024-10-28 22:01:50 +0800

    feat: try other tokenizer
    Signed-off-by: Mingzhuo Yin <yinmingzhuo@gmail.com>

--- src/guc.rs
+++ src/guc.rs
@@ -1,6 +1,7 @@
 use pgrx::{GucContext, GucFlags, GucRegistry, GucSetting};
 
 pub static BM25_LIMIT: GucSetting<i32> = GucSetting::<i32>::new(100);
+pub static ENABLE_INDEX: GucSetting<bool> = GucSetting::<bool>::new(true);
 
 pub unsafe fn init() {
     GucRegistry::define_int_guc(
@@ -10,6 +11,14 @@ pub unsafe fn init() {
         &BM25_LIMIT,
         1,
         65535,
+        GucContext::Userset,
+        GucFlags::default(),
+    );
+    GucRegistry::define_bool_guc(
+        "bm25_catalog.enable_index",
+        "bm25 index enable closure",
+        "Whether to enable the bm25 index",
+        &ENABLE_INDEX,
         GucContext::Userset,
         GucFlags::default(),
     );
